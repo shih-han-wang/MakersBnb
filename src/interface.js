031c2ddc04049125c5465f004d4d62fb96c2cf83
North <blarvinius@gmail.com>
2018-04-24 13:35:26 +0100

    change file structure (move jasmine)

--- src/interface.js
+++ src/interface.js
@@ -2,4 +2,19 @@ $(document).ready(function(){
 
 
 
+  $("#add-houses").submit(function(event){
+      event.preventDefault();
+
+      var name = $("#house-name").val();
+      var date1 = $("#dates").val();
+      var price = $("#price").val();
+
+      
+
+
+
+      $("#houses").append("<li>" + name + "  " + dates + "  " + price + "  " + "</li>");
+    });
+
+
 });
