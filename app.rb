48760650ff900fcfd47c47fcdbc8de6963316478
North <blarvinius@gmail.com>
2018-04-26 16:48:24 +0100

    pull from master

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require './lib/property'
+
+
+class Makersbnb < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/properties' do
+    @properties = Property.all
+    erb :properties
+  end
+
+  get '/addproperty' do
+    erb :addproperty
+  end
+
+  post '/adding' do
+    p params
+    Property.create(username: params[:username], property: params[:property], description: params[:description], price: params[:price])
+    redirect '/properties'
+  end
+
+
+  run! if app_file == $0
+end
---
+++ spec/feature/home_spec.rb
@@ -0,0 +1,48 @@
+feature 'home page content' do
+  scenario 'welcomne' do
+    visit '/'
+    expect(page).to have_content 'Welcome!'
+  end
+end
+
+feature 'button takes user to properties page' do
+  scenario 'properties page' do
+    visit '/'
+    click_button 'View Properties'
+    expect(page).to have_content 'Available Properties'
+  end
+end
+
+feature 'button takes user to adding properties page' do
+  scenario 'add property' do
+    visit '/properties'
+    click_button 'Add Property'
+    expect(page).to have_content 'Enter your property details here'
+  end
+end
+
+feature 'form to add properties' do
+  scenario 'submit button' do
+    visit '/addproperty'
+    expect(page).to have_button 'submit'
+  end
+end
+
+feature 'form to add properties' do
+  scenario 'see a text entry field' do
+    visit '/addproperty'
+    expect(page).to have_field 'property'
+  end
+end
+
+feature 'add property and see the display' do
+  scenario '' do
+    visit 'addproperty'
+    fill_in('username', :with => 'abc')
+    fill_in('property', :with => 'Lovely Home')
+    fill_in('description', :with => 'This is a very beautiful house')
+    fill_in('price', :with => '55')
+    click_button 'submit'
+    expect(page). to have_content 'Lovely Home'
+  end
+end
