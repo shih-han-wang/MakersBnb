3b926f41c6a66e487529085c3f127a5bc0878c6c
North <blarvinius@gmail.com>
2018-04-26 12:12:55 +0100

    changing directions

--- spec/someCrapSpec.js
+++ spec/someCrapSpec.js
@@ -10,33 +10,66 @@ describe('Houses', function() {
     });
   });
 
-  describe('_houseList', function() {
-    it('starts as an empty object', function() {
-      myHouses._houseList.fart = {smell: "awful"};
-      expect(myHouses._houseList.fart).toEqual({smell: "awful"});
-    });
-  });
+  // describe('_houseList', function() {
+  //   it('starts as an empty object', function() {
+  //     myHouses._houseList.fart = {smell: "awful"};
+  //     expect(myHouses._houseList.fart).toEqual({smell: "awful"});
+  //   });
+  // });
 
-  describe('_houseList', function() {
-    it('starts as an empty object', function() {
-      myHouses._houseList.fart = {smell: "awful"};
-      expect(myHouses._houseList.fart.smell).toBe("awful");
-    });
-  });
+  // describe('_houseList', function() {
+  //   it('starts as an empty object', function() {
+  //     myHouses._houseList.fart = {smell: "awful"};
+  //     expect(myHouses._houseList.fart.smell).toBe("awful");
+  //   });
+  // });
+
+  // describe('addHouse', function() {
+  //   it('adds keys and objects', function() {
+  //     myHouses.addHouse("Mansion");
+  //     expect(myHouses._houseList.Mansion).toEqual({descrip: null, price: null, dates: []});
+  //   });
+  // });
 
   describe('addHouse', function() {
-    it('adds keys and objects', function() {
+    it('adds NAME and empty object {}', function() {
       myHouses.addHouse("Mansion");
-      expect(myHouses._houseList.Mansion).toEqual({descrip: null, price: null, dates: []});
+      expect(myHouses._houseList.Mansion).toEqual({});
     });
   });
 
-  describe('addHouse', function() {
-    it('can add description', function() {
-      myHouses.addHouse('Mansion', {description: "Uuuuge, very Uuuge"});
-      expect(myHouses._houseList.Mansion.descrip).toEqual("Uuuuge, very Uuuge");
+  describe('addPrice', function() {
+    it('adds price to existing house', function() {
+      myHouses.addHouse("Shack");
+      myHouses.addPrice(Shack, 400);
+      expect(myHouses._houseList.Shack.price).toBe(400);
     });
   });
+
+  // describe('addHouse', function() {
+  //   it('can add description', function() {
+  //     myHouses.addHouse('Mansion', {description: "Uuuuge, very Uuuge"});
+  //     expect(myHouses._houseList.Mansion.descrip).toBe("Uuuuge, very Uuuge");
+  //   });
+  // });
+
+  // describe('addHouse', function() {
+  //   it('can add any number of dates (CSVs)', function() {
+  //     myHouses.addHouse('Mansion', {dates: ['1/2/3', '2/3/4', '3/4/5']});
+  //     expect(myHouses._houseList.Mansion.dates).toEqual(['1/2/3', '2/3/4', '3/4/5']);
+  //   });
+  // });
+
+  // describe('addHouse', function() {
+  //   it('can add MORE dates (CSVs)', function() {
+  //     myHouses.addHouse('Mansion', {dates: ['1/2/3', '2/3/4', '3/4/5']});
+  //     myHouses.addHouse('Mansion', {dates: ['4/5/6', '5/6/7', '6/7/8']});
+  //     expect(myHouses._houseList.Mansion.dates).toEqual(['1/2/3', '2/3/4', '3/4/5', '4/5/6', '5/6/7', '6/7/8' ]);
+  //   });
+  // });
+
+
+
 
   // describe('increase', function() {
   //   it('increases by 10', function() {
--- src/someCrap.js
+++ src/someCrap.js
@@ -2,9 +2,20 @@
 
 function Houses() {
   this._houseList = {};
+
 }
 
 
-Houses.prototype.addHouse = function( name = null, {description = null, price = null, dates = []} = {}) {
+// Houses.prototype.addHouse = function( name = null, {description = null, price = null, dates = []} = {}) {
+//   // var dateArray = [];
+//   // for (var i in dates) dateArray.push(dates[i]);
   this._houseList[name] = {descrip: description, price: price, dates: dates};
+// };
+
+Houses.prototype.addHouse = function( name = null) {
+  this._houseList[name] = {};
+};
+
+Houses.prototype.addPrice = function(name, price) {
+  this._houseList[name.price] = price;
 };
